16cfd9d66810b3affb12c83780a06e9f27aadb81
Thenujan-Dev <thenujanc@gmail.com>
2025-07-15 11:08:06 +0530

    feat(login): add login page with centered LoginForm component

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import LoginForm from "./LoginForm";
+
+const page = () => {
+  return (
+    <section className="flex h-dvh w-full flex-col items-center justify-center">
+      <div className="mx-auto w-full max-w-xs">
+        <LoginForm />
+      </div>
+    </section>
+  );
+};
+
+export default page;
